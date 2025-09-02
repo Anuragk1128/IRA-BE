43cfaa6b4e731e34e6eb08d62ac9eb83cdcbd3bb
Anuragk1128 <admin@houseofevolve.in>
2025-09-01 23:35:50 -0700

    cl

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,12 @@
-import { Cloudinary    } from "cloudinary";
-const cloudinary = Cloudinary.v2;   
+const dotenv = require("dotenv");
+dotenv.config();
+
+const cloudinary = require("cloudinary").v2;
+
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+module.exports = cloudinary;
